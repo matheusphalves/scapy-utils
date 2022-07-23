88c3dc2168b9fa7e0bba83ea972a783fe8d86fe7
Matheus Phelipe <matheusphalves@gmail.com>
2022-07-23 12:32:15 -0300

    adding logs in ArpScanner

--- scapy-utils/operations/ArpScanner.py
+++ scapy-utils/operations/ArpScanner.py
@@ -5,7 +5,7 @@ class ArpScanner:
 
     @staticmethod
     def send_arp_packet( 
-    target_ip = '192.168.1.1/24', 
+    target_ip = '192.168.5.1/24', 
     ether_dst = 'ff:ff:ff:ff:ff:ff',
     timeout = 5):
         try:
@@ -28,4 +28,22 @@ class ArpScanner:
             })
         return clients
 
-ArpScanner.send_arp_packet(target_ip='123Banana')
+    @staticmethod
+    def start_scan(target_ip_list, timeout = 5):
+        ip_addr_report_list = {}
+        for ip_addr in target_ip_list:
+            try:
+                print(f"{ip_addr} - STARTING SCAN")
+                arp_result = ArpScanner.send_arp_packet(ip_addr, timeout=timeout)
+                clients = ArpScanner.get_clients(arp_result[0])
+            except Exception as ex:
+                    print(f"{ip_addr} - SCAN FAILED: {str(ex)}")
+                    break
+
+            print(f"{ip_addr} - SCAN SUCCESSFULLY")
+            ip_addr_report_list[ip_addr] = {"clients": clients}
+
+        return ip_addr_report_list
+
+
+print(ArpScanner.start_scan(['192.168.5.1/24', '192.168.1.1/24']))
