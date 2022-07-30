917e89c3830e377fcedd14bced754f2c1df43876
Matheus Phelipe <matheusphalves@gmail.com>
2022-07-30 11:23:50 -0300

    resolve exception import module

---
+++ scapy4dummy/test/test.py
@@ -0,0 +1,4 @@
+#from operations.ArpScanner import ArpScanner
+#from . import PortScanner
+#print(ArpScanner.start_scan(['192.168.5.1/24', '192.168.1.1/24']))
+#print(PortScanner.start_full_scan(['127.0.0.1'], [3389, 3306], verbose=False))
